57450d8f3417af4dd2673bf65fea4ed6db45fb60
dineshreddymarri <dmarr2@newhaven.edu>
2015-07-01 12:25:23 -0400

    Create IntegerDivision.py

---
+++ IntegerDivision.py
@@ -0,0 +1,18 @@
+from random import randrange
+print (" Interger Division")
+while(1):
+    a= randrange(20)
+    b= randrange(5)
+    try:
+        ans = a/b
+    except:
+        ans = -1
+    print ("",a,"/",b,"=",)
+    number = input("")
+    try:
+        if int(ans) == int(number):
+            print ("Correct")
+        else:
+            print("Incorrect")
+    except ValueError:
+        print("please enter integers Only")
